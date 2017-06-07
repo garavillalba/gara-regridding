655278bbe2247392129ecee9d63bd7b78de7b246
Ryan O'Kuinghttons <ryan.okuinghttons@noaa.gov>
2017-06-07 13:07:10 -0600

    fixup

--- modify_CASA.py
+++ modify_CASA.py
@@ -2,23 +2,22 @@
 import ocgis
 
 # wrfout file may need to be renamed to have a .nc ending for ocgis to recognize the format
-# PATH_IN = "/software/co2flux/SurfaceFluxData/CASA/GEE.3hrly.1x1.25.2015.nc"
+PATH_IN = "/software/co2flux/SurfaceFluxData/CASA/GEE.3hrly.1x1.25.2015.nc"
+# PATH_IN = '/home/ryan/sandbox/gara-regridding/GEE.3hrly.1x1.25.2015.nc'
 
-PATH_IN = '/home/ryan/sandbox/gara-regridding/GEE.3hrly.1x1.25.2015.nc'
-
-# PATH_OUT = "/software/co2flux/SurfaceFluxData/CASA/modified_GEE.3hrly.1x1.25.2015.nc"
-PATH_OUT = 'modified_GEE.3hrly.1x1.25.2015.nc'
+PATH_OUT = "/software/co2flux/SurfaceFluxData/CASA/modified_GEE.3hrly.1x1.25.2015.nc"
+# PATH_OUT = 'modified_GEE.3hrly.1x1.25.2015.nc'
 
 rd = ocgis.RequestDataset(PATH_IN)
 vc = rd.get_variable_collection()
 
 lat = vc["lat"]
 lat.attrs["units"] = "degrees_north"
-lat = vc["lon"]
-lon.attrs["units"] = "degrees_east"
+lon = vc["lon"]
+lon.attrs["units"] = "degrees"
 
 # now create a grid and write bounds
-grid = ocgis.Grid(sub['lon'].extract(), sub['lat'].extract())
+grid = ocgis.Grid(lon, lat, crs=ocgis.crs.Spherical())
 grid.set_extrapolated_bounds('bounds_lat', 'bounds_lon', 'corners')
 grid.x.bounds.attrs.pop('units')
 grid.y.bounds.attrs.pop('units')
